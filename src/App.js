fc0d44fd224351d39c0f92db9a3bbf21096b9a04
liling0726 <1329301856@qq.com>
2018-10-17 20:53:20 +0800

    修改transition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import {CSSTransition,TransitionGroup} from 'react-transition'
+import React, {
+  Component
+} from 'react';
+
+import {
+  Transition
+} from 'react-transition-group'
 import './App.css';
 
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+}
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered:  { opacity: 1 },
+};
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false,
+    }
+  }
+  componentDidMount(){
+    this.setState({
+      show:true
+    })
+  }
   render() {
-    return (
-      <div className="App">
-       
+    const { show }  = this.state
+    return ( <div className = "App" >
+    <button
+          onClick={() => {
+            this.setState(state => ({
+              show: !state.show,
+            }));
+          }}
+        >
+          Toggle
+        </button>
+        <Transition in={show} timeout={duration}>
+    {(state) => (
+      <div style={{
+        ...defaultStyle,
+        ...transitionStyles[state]
+      }}>
+        I'm a fade Transition!
       </div>
+    )}
+  </Transition>
+       </div>
     );
   }
 }

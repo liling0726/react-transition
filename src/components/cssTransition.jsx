a98e39f3368381484ced5a7ff0985248fac3b6ab
liling0726 <1329301856@qq.com>
2018-10-22 19:57:26 +0800

    csstransition

--- src/components/cssTransition.jsx
+++ src/components/cssTransition.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { CSSTransition } from 'react-transition-group'
-export default class CSSTransitionItem extends React.Component{
- render(){
+import './cssTransition.less'
+export default class CSSTransitionItem extends React.Component {
+  state = {
+    showImg:false
+  }
+  handleToggle=()=>{
+    this.setState(state => ({
+      showImg: !state.showImg,
+    }))
+};
+  render() {
    return <div>
-     CSSTransition
-   </div>
- }
+      <button onClick={this.handleToggle}>切换</button>
+      <CSSTransition  classNames="img"
+      in={this.state.showBox}
+        onEnter={() => {
+          this.boxDOM.style.display = 'block'
+        }}
+        onExited={() => {
+          this.boxDOM.style.display = 'none'
+        }}
+        timeout={300}
+      >
+        <div className="box"
+        ref="box">
+          <h1>测试动画效果滴。</h1>
+          <div className="color-box">哈哈哈</div>
+        </div>
+      </CSSTransition>
+    </div>
+  }
 }
